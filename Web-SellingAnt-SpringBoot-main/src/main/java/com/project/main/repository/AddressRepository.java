3472c5101903c77984dc08d2c08527ccbf62d010
kunita09 <137262738+kunita09@users.noreply.github.com>
2024-02-21 11:18:14 +0700

    Delete Web-SellingAnt-SpringBoot-main directory

--- Web-SellingAnt-SpringBoot-main/src/main/java/com/project/main/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.main.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.main.entity.Address;
-
-public interface AddressRepository extends JpaRepository<Address, Integer> {
-    // You can define custom query methods here if needed
-}
